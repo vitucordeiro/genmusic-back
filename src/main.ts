93e41e64d31707f05cf531bcd21626a3dddf46a0
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-12 18:02:19 +0000

    feat(geminiAi and Swagger): add configurations to enable Swagger and gemini API

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './App/app.module';
+import * as compression from 'compression';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { env } from './commom/env.config';
+import { validateConfig } from './commom/validate.config';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
+function setupSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('Gemini example')
+    .setDescription('The Gemini API description')
+    .setVersion('1.0')
+    .addTag('google gemini')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.useGlobalPipes(validateConfig);
+  app.use(compression());
+  setupSwagger(app);
+  await app.listen(env.PORT);
 }
 bootstrap();
