8adbe47d5fdf6442ff7d71dcd8d73313c2b1cd41
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-12 18:09:36 +0000

    refactor(geminiAI): renaming import folder

--- src/modules/gemini/test/gemini.service.spec.ts
+++ src/modules/gemini/test/gemini.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { GeminiService } from './gemini.service';
+import { GeminiService } from '../services/gemini.service';
 
 describe('GeminiService', () => {
   let service: GeminiService;
