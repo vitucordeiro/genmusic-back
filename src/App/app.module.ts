145d7da405018930385909cf3e971efd96fa3eed
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-31 16:34:09 -0300

    refactor: module app -> including new modules and excluding unecessarily objects on module core

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -7,6 +7,10 @@ import {
 import { NestMiddleware } from '@nestjs/common';
 import { json, urlencoded } from 'body-parser';
 import { GeminiModule } from '../modules/gemini/gemini.module';
+import { SpotifyModule } from 'src/modules/spotify/spotify.module';
+import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
+import { AppController } from './app.controller';
 
 export class JsonBodyParserMiddleware implements NestMiddleware {
   use = json({ limit: '100kb' });
@@ -16,9 +20,8 @@ export class UrlEncodedParserMiddleware implements NestMiddleware {
   use = urlencoded({ extended: true });
 }
 @Module({
-  imports: [GeminiModule],
-  controllers: [],
-  providers: [],
+  imports: [GeminiModule, SpotifyModule, ConfigModule, HttpModule],
+  controllers: [AppController],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
