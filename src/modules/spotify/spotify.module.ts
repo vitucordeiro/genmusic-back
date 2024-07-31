a9b4253c0911f9220a0ec0dc9eb948ce5d97ac69
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-31 16:26:09 -0300

    fix: module spotify -> including SpotifyService on module

--- src/modules/spotify/spotify.module.ts
+++ src/modules/spotify/spotify.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { SpotifyService } from './services/spotify.service';
 
-@Module({})
+@Module({
+  imports: [HttpModule],
+  providers:[SpotifyService],
+  exports:[SpotifyService]
+})
 export class SpotifyModule {}
