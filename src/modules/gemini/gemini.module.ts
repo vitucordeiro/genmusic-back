29240c6a737f97a4cb43df01795ce390cc959849
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-07-31 16:26:54 -0300

    import { GeminiController } from './controllers/gemini.controller';

--- src/modules/gemini/gemini.module.ts
+++ src/modules/gemini/gemini.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GeminiController } from './controllers/gemini.controller';
 import { GeminiService } from './services/gemini.service';
 
 @Module({
-  controllers: [GeminiController],
   providers: [GeminiService],
+  exports:[GeminiService]
 })
 export class GeminiModule {}
