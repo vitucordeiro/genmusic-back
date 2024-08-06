305140375df63c5eccac50b98895126bdd9a9b5e
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-08-06 13:58:39 -0300

    feat: config CORS

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,12 @@ function setupSwagger(app) {
 }
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3001', // URL do seu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(validateConfig);
   app.use(compression());
   setupSwagger(app);
