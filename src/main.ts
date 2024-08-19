4cd6284bb4fbf8d2a0f05f0a301cba09146937c9
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2024-08-19 15:58:40 -0300

    feature: add more origin urls on CORS

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ function setupSwagger(app) {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // URL do seu frontend
+    origin: ['http://localhost:3001', 'http://localhost:3000' ], // URL do seu frontend
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
